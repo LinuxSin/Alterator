109c2501d1cf79a8a33d64e1107042e7e83267c3
LinuxSin <mr-zo99@yandex.ru>
2025-11-12 15:35:38 +0300

    Добавил выпадающий список

--- ajax.scm
+++ ajax.scm
@@ -3,6 +3,31 @@
   :use-module (alterator ajax)
   :export (init))
 
+; ////////////////////////////////////////////////////////////////////////////
+;; Функция для получения списка профилей
+(define (get-profiles-list)
+  (woo-list "/m104/profiles"))
+
+;; Функция для смены профиля
+;; Функция для смены профиля
+;; Функция для смены профиля
+(define (ui-change-profile)
+  (catch/message
+   (lambda()
+     (let ((selected-profile (form-value "profiles")))
+       (when selected-profile
+         ;; Устанавливаем выбранный профиль
+         (woo "set_profile" "/m104" 'profile_name selected-profile)
+         ;; Обновляем таблицу с новыми данными
+         (let ((hosts-data (woo-list "/m104/hosts")))
+           (form-update-enum "hosts" hosts-data)
+           ;; Обновляем заголовок из первого элемента (если есть)
+           (when (not (null? hosts-data))
+             (let ((first-host (car hosts-data)))
+               (form-update-value "title" (woo-get-option first-host 'title))))))))))
+
+; /////////////////////////////////////////////////////////////////////////////
+
 
 (define (ui-edit-profile)
 (form-replace "/m104/edit"))
@@ -88,18 +113,31 @@
              (woo-list "/m104/hosts"))))))))
 
 
-;; Основная функция
 (define (init)
   (catch/message (lambda ()
-    (form-update-enum "hosts"
-      (woo-list "/m104/hosts"))))
+    ;; Загружаем список профилей
+    (let ((profiles-list (get-profiles-list)))
+      (form-update-enum "profiles" profiles-list)
+      
+      ;; Пытаемся установить m104 как выбранный, если он есть в списке
+      (if (member "m104" (map (lambda (x) (woo-get-option x 'name)) profiles-list))
+          (form-update-value "profiles" "m104")
+          ;; Иначе выбираем первый элемент
+          (when (not (null? profiles-list))
+            (form-update-value "profiles" (woo-get-option (car profiles-list) 'name))))
+      
+      ;; Загружаем данные хостов и обновляем заголовок
+      (let ((hosts-data (woo-list "/m104/hosts")))
+        (form-update-enum "hosts" hosts-data)
+        ;; Устанавливаем заголовок из первого элемента (если есть)
+        (when (not (null? hosts-data))
+          (let ((first-host (car hosts-data)))
+            (form-update-value "title" (woo-get-option first-host 'title))))))))
   
+  (form-bind "profiles" "click" ui-change-profile)  
   (form-bind "datadd" "click" ui-datadd)
   (form-bind "startBtn" "click" ui-startBtn)
   (form-bind "stopBtn" "click" ui-stopBtn)
   (form-bind "restartBtn" "click" ui-restartBtn)
-
-  (form-bind "redact_prof" "click" ui-edit-profile)
-
-  
+  (form-bind "redact_prof" "click" ui-edit-profile)  
 )
