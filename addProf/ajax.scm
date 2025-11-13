3510b644dd3a94c17be66d3259238ed5c2476bb8
LinuxSin <mr-zo99@yandex.ru>
2025-11-13 16:11:51 +0300

    Добавил функцию создания нового профиля

--- addProf/ajax.scm
+++ addProf/ajax.scm
@@ -3,11 +3,22 @@
   :use-module (alterator ajax)
   :export (init))
 
-
-
 (define (ui-return)
 (form-replace "/m104/edit"))
 
+(define (ui-create-profile)
+  (catch/message
+   (lambda()
+     (let ((new-profile-name (form-value "new_profile_name")))
+       (when (and new-profile-name (not (string-null? new-profile-name)))
+         ;; Вызываем метод создания профиля
+         (woo "create_profile" "/m104" 'profile_name new-profile-name)
+         ;; Устанавливаем новый профиль как текущий
+         (woo "set_profile" "/m104" 'profile_name new-profile-name)
+         ;; Возвращаемся к странице редактирования с уже выбранным новым профилем
+         (form-replace "/m104/edit"))))))
+
 (define (init)
-  (form-bind "return" "click" ui-return)  
+  (form-bind "return" "click" ui-return)
+  (form-bind "create_profile" "click" ui-create-profile)  
 )
